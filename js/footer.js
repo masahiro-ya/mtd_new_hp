e5efebce21cbfecc3ed3075e5ea94e7ffecbce18
masahiro_ya <abcabcmart5644@gmail.com>
2022-09-27 15:54:46 +0900

    first commit

---
+++ js/footer.js
@@ -0,0 +1,55 @@
+document.write('<footer>');
+
+document.write('<div class="footer-index container">');
+document.write('<div class="footer-box f-box1">');
+document.write('<h2 class="footer-title">企業案内</h2>');
+document.write('<div class="footer-nav">');
+document.write('<ul class="footer-ul">');
+document.write('<li class="footer-li"><a href="greeting.html">社長挨拶</a></li>');
+document.write('<li class="footer-li"><a href="philosophy.html">企業理念</a></li>');
+document.write('<li class="footer-li"><a href="overview.html">会社概要</a></li>');
+document.write('<li class="footer-li"><a href="history.html">沿革</a></li>');
+document.write('</ul>');
+document.write('</div>');
+document.write('</div>');
+document.write('<div class="footer-box f-box2">');
+document.write('<h2 class="footer-title">事業案内</h2>');
+document.write('<div class="footer-nav">');
+document.write('<ul class="footer-ul">');
+document.write('<li class="footer-li"><a href="service.html">事業内容</a></li>');
+document.write('</ul>');
+document.write('</div>');
+document.write('</div>');
+
+document.write('<div class="copyright">');
+document.write('<p class="p-copyright"><small>Copyright &copy; MTD株式会社 All Rights Reserved.</small></p>');
+document.write('</div>');
+
+document.write('</div>');
+
+document.write('</footer>');
+
+
+//ページトップに戻る
+// jQuery(document).ready(function() {
+// var offset = 100;
+// var duration = 500;
+// jQuery(window).scroll(function() {
+// if (jQuery(this).scrollTop() > offset) {
+// jQuery('.pagetop').fadeIn(duration);
+// } else {
+// jQuery('.pagetop').fadeOut(duration);
+// }
+// });
+
+// jQuery('.pagetop').click(function(event) {
+// event.preventDefault();
+// jQuery('html, body').animate({scrollTop: 0}, duration);
+// return false;
+// })
+// });
+
+
+
+
+
